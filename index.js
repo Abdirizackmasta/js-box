449560e86bbe48f67647f08e4ff30c1585a29103
Abdirizackmata <abdirizackissack2018@gmail.com>
2024-07-12 13:06:24 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Day 1
+let string1 = "JavaScript";
+console.log(string1.substr(4, 6)); // Script
+
+let country = "Finland";
+
+let string2 = "JavaScript";
+
+console.log(string2.substring(0, 4)); // Java
+console.log(string2.substring(4, 10)); // Script
+console.log(string2.substring(4)); // Script
